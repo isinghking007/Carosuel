840a36949fa4c822d444a186733e099ae320a24f
isinghking007 <007ravinash@gmail.com>
2023-08-20 20:49:37 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Carosuel';
+
+  images=[
+    {
+      ImageSrc:'https://source.unsplash.com/78A265wPiO4',
+      ImageAlt:'First Image'
+    },
+    
+    {
+      ImageSrc:'https://source.unsplash.com/DlkF4-dbCOU',
+      ImageAlt:'Second Image'
+    },
+    
+    {
+      ImageSrc:'https://source.unsplash.com/oMneOBYhJxY',
+      ImageAlt:'Third Image'
+    },
+    
+    {
+      ImageSrc:'https://source.unsplash.com/9Wotadyt7K8',
+      ImageAlt:'Fourth Image'
+    },
+    
+    {
+      ImageSrc:'https://source.unsplash.com/9KeDcJKEzBY',
+      ImageAlt:'Fifth Image'
+    }
+  ]
 }
---
+++ src/app/carosuel/carosuel.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarosuelComponent } from './carosuel.component';
+
+
+
+@NgModule({
+  declarations: [
+    CarosuelComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CarosuelComponent
+  ]
+})
+export class CarosuelModule { }
